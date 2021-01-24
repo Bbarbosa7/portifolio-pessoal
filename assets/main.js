68f9cc5eff1207028d541ed91895b2024e876a01
Bbarbosa7 <brayann.wheberth@gmail.com>
2021-01-24 13:37:41 -0300

    Tranferencia para novo diretório Assets

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// Menu mobile
+$(function() {
+
+   $('.main_header_content_menu_mobile_obj').on('click', function(){
+      $('.main_header_content_menu_mobile_sub').toggleClass('ds_none');
+      $(this).toggleClass('main_header_content_menu_mobile_obj_active');
+   });
+});
+
+// Lazyload
+$("img.lazy").lazyload({effect : "fadeIn"});
